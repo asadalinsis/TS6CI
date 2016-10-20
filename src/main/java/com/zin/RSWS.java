85f8394e348544892760b962e9bdcf4e2ea56129
Asad.Ali <Asad.Ali@ZIN-062.ZINTECHNOLOGIES.COM>
2016-10-20 16:54:18 +0500

    My First commit

---
+++ src/main/java/com/zin/RSWS.java
@@ -0,0 +1,74 @@
+package com.zin;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class RSWS {
+	private static List<User> listUsers = new ArrayList<User>();
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/ping")
+	public Response ping()
+	{
+		String output = "Webservice is working";
+		return Response.status(200).entity(output).build();
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/addUser")
+	public Response addUser(User user)
+	{
+		String output = "";
+		///check if user already exists
+		boolean alreadyExists = listUsers.contains(user);
+		
+		if(alreadyExists){
+			output = "User Already exists";
+			return Response.status(200).entity(output).build();
+			
+		}else{
+			boolean added = listUsers.add(user);
+			output = "User Successfully Added";
+			return Response.status(200).entity(output).build();
+		}
+		
+	}
+	
+	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/removeUser")
+	public Response removeUser(User user)
+	{
+		String output = "";
+		///check if user already exists
+		boolean alreadyExists = listUsers.contains(user);
+		if(alreadyExists){
+			boolean removed = listUsers.remove(user);
+			if(removed){
+				output = "User Successfully Removed";
+				return Response.status(200).entity(output).build();
+			}else{
+				output = "User not removed please check with Administrator";
+				return Response.status(200).entity(output).build();
+			}
+		}else{
+			output = "User Does not exist";
+			return Response.status(200).entity(output).build();
+		}
+	}
+
+}
---
+++ src/main/java/com/zin/User.java
@@ -0,0 +1,46 @@
+package com.zin;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "User", namespace = "com.zin.User")
+@XmlRootElement(name = "User")
+public class User {
+	private String firstName;
+	private String lastName;
+	
+	public User() {
+	}
+	
+	@XmlElement(name = "FirstName")
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	@XmlElement(name = "LastName")
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("equals called with "+obj);
+		boolean isSame = false;
+        if (obj != null && obj instanceof User){
+        	isSame = this.getFirstName().equals(((User)obj).getFirstName()) && this.getLastName().equals(((User)obj).getLastName());
+        }
+
+        return isSame;
+	}
+	@Override
+	public String toString() {
+		return "{\"FirstName\":"+firstName+",\"LastName\":"+lastName+"}";
+	}
+}
